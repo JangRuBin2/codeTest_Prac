44414fed6da46960cdea401e1dfe1b9d02351544
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-01 12:55:53 +0900

    배열 값 이상한 것 수정

--- minimumSwap.js
+++ minimumSwap.js
@@ -4,10 +4,14 @@ function minimumSwaps(arr) {
   const centerIndex = Math.floor(n/2);
   const centerNum = arr[centerIndex];
   const leftNum = [...arr.slice(0, centerNum+1)];
+  let nowIndex = 0;
+  let nowArray = arr;
   for(let i=0; i<leftNum.length; i++) {
     if(leftNum[i] > centerNum) {
-      return [centerNum, ...leftNum, ...arr.slice(-centerIndex)]
+      swapCount++;
+      nowArray = [centerNum, ...leftNum.slice(1), arr[0],...arr.slice(-centerIndex)];
     }
-  }
+  };
+  return nowArray
   }
   console.log(minimumSwaps([7, 1, 3, 2, 4, 5, 6]))
