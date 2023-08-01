55de4a9cfd3ae4afb21cbb554af1d680ef489978
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-01 12:08:53 +0900

    slice메서드 확인

--- pairSocks.ts
+++ pairSocks.ts
@@ -1,1 +1,66 @@
-console.log('하이');
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+function countingValleys(steps: number, path: string): any {
+    // Write your code here
+    let valleyCount = 0;
+    let level = 0;
+    
+    for(const step of path) {
+        if ( step === 'U') {
+            level++;
+        } else if (step === 'D') {
+            level--;
+        }
+        if (level ===0 && step ==='U') {
+            valleyCount++;
+        }
+    }
+    return valleyCount;
+  
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH'] as string);
+
+    const steps: number = parseInt(readLine().trim(), 10);
+
+    const path: string = readLine();
+
+    const result: number = countingValleys(steps, path);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
---
+++ slice.js
@@ -0,0 +1,3 @@
+const testArray = [1, 2, 3, 4, 5];
+const subArray1 = testArray.slice(1,-1);
+console.log(subArray1);
