630d22a55df1c6caaf8d96742e537bcd28f369df
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-01 18:08:57 +0900

    q[i]번째 요소에 원래 올 숫자가 아니면 Chaotic반환

--- newYearChaos.js
+++ newYearChaos.js
@@ -3,15 +3,15 @@ function minimumBribes(q) {
   const n = q.length;
   let bribes = 0;
   for (let i=0; i<n; i++) {
-    //만약 q[i]요소가 원래 위치에 있어야 할 숫자보다 크다면 뇌물을 한번 줘서 2칸 뒤로 이동
-    let temp = q[i];
-    if(temp > i+1) {
-      temp-2
+    //만약 배열의 i번째 index가 원래 있어야 할 숫자가 아니라면
+    while (q[i] === i+1) {
       bribes++;
+      if(bribes > 2) {
+        console.log('Too Chaotic');
+        return;
+      }
     }
-  } if(bribes > 2) {
-    console.log('Too Chaotic')
-  }
-  console.log(bribes);
+    
+  };
 }
 minimumBribes([1,7,2,5,6,4])
