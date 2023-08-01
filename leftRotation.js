8a5f3a391e229bf40730b142ef8ac5ef8a61bc60
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-01 19:46:43 +0900

    하나의 변수로 최적화

--- leftRotation.js
+++ leftRotation.js
@@ -1,7 +1,6 @@
 function leftRot(a, d) {
   const n = a.length % d;
-  const left =[...a].slice(n);
-  const right = [...a].slice(0,n)
-  return [...left,...right]
+  const array = [...a.slice(n),...a.slice(0,n)]
+  return array
 }
 console.log(leftRot([1,2,3,4,5],3))
