30c87cf12dad0ad6d407c2bd9248034cb300fc48
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-01 12:18:52 +0900

    left Rotate 문제

--- slice.js
+++ slice.js
@@ -1,3 +1,10 @@
 const testArray = [1, 2, 3, 4, 5];
 const subArray1 = testArray.slice(1,-1);
-console.log(subArray1);
+function rotLeft(a, d) {
+  // Write your code here
+const n = d % a.length;
+const circularArray = [...a.slice(n), ...a.slice(0,n)];
+return circularArray
+
+}
+console.log(rotLeft([1,2,3,4,5], 2))
