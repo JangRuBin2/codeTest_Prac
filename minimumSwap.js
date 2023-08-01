1556dcbd0033e2a5975623c0422e2eeaa353d692
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-01 12:31:38 +0900

    가운데 숫자 구하기

---
+++ minimumSwap.js
@@ -0,0 +1,8 @@
+function minimumSwaps(arr) {
+  const n = arr.length;
+  let swapCount = 0;
+  const centerNum = [...arr.slice(n/2)][0];
+  
+  return leftNum;
+  }
+  console.log(minimumSwaps([7, 1, 3, 2, 4, 5, 6]))
