1a9575dc104f97ac0c54b3a3f6169bfb28f2e23e
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-01 14:56:27 +0900

    배열은 1~n으로 순차적으로 증가

--- minimumSwap.js
+++ minimumSwap.js
@@ -1,26 +1,19 @@
 function minimumSwaps(arr) {
   const n = arr.length;
   let swapCount = 0;
-  const centerIndex = Math.floor(n/2);
-  const centerNum = arr[centerIndex];
-  const leftNum = [...arr.slice(0, centerNum+1)];
-  let nowIndex = 1;
-  let nowArray = arr;
-  for(let i=0; i<leftNum.length; i++) {
-    if(leftNum[i] > centerNum) {
+
+  for (let i = 0; i < n - 1; i++) {
+    while (arr[i] !== i + 1) {
+      const temp = arr[i];
+      arr[i] = arr[temp - 1];
+      arr[temp - 1] = temp;
       swapCount++;
-      nowArray = [centerNum, ...leftNum.slice(1), arr[0],...arr.slice(-centerIndex)];
-    } for(let j=0; j<nowArray.length; j++) {
-      if(nowArray[j]> nowArray[j+nowIndex]) {
-        //재할당
-        [nowArray[j],nowArray[nowIndex]] = [nowArray[nowIndex], nowArray[j]];
-        swapCount++;
-        
-      } else if(nowArray[j]< nowArray[j+nowIndex]) {
-        return nowArray;
-      }
     }
-  };
-  return nowArray
   }
-  console.log(minimumSwaps([7, 1, 3, 2, 4, 5, 6]))
+
+  return swapCount;
+}
+
+// 예시 배열을 사용하여 함수를 테스트합니다.
+const arr = [7, 1, 3, 2, 4, 5, 6];
+console.log(minimumSwaps(arr)); // 출력 결과: 5 (최소 스왑 횟수)
