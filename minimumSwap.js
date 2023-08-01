b05c8be67a6e195f0c33585c891781f67cb5b81d
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-01 12:42:50 +0900

    조건 테스트

--- minimumSwap.js
+++ minimumSwap.js
@@ -4,6 +4,12 @@ function minimumSwaps(arr) {
   const centerIndex = Math.floor(n/2);
   const centerNum = arr[centerIndex];
   const leftNum = [...arr.slice(0, centerNum+1)];
+  for(let i=0; i<leftNum.length; i++) {
+    if(leftNum[i] > centerNum) {
+      console.log(`${leftNum[i]}는 ${centerNum}보다 큽니다`);
+      break;
+    }
+  }
   return leftNum;
   }
   console.log(minimumSwaps([7, 1, 3, 2, 4, 5, 6]))
