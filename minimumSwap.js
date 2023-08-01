53017c757b3738b45c2e13d141b3ae3f44007623
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-01 12:40:17 +0900

    array의 가운데 값을 가져오는데는 성공, leftNum이 원하는대로 안가져와짐

--- minimumSwap.js
+++ minimumSwap.js
@@ -1,8 +1,9 @@
 function minimumSwaps(arr) {
   const n = arr.length;
   let swapCount = 0;
-  const centerNum = [...arr.slice(n/2)][0];
-  
+  const centerIndex = Math.floor(n/2);
+  const centerNum = arr[centerIndex];
+  const leftNum = [...arr.slice(0, centerNum)];
   return leftNum;
   }
   console.log(minimumSwaps([7, 1, 3, 2, 4, 5, 6]))
