b864cf1f9dcb79054680d4c93e06c74956f62769
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-02 11:11:33 +0900

    간단한 테스트를 통해 이해 완료

--- minimumSwap2.js
+++ minimumSwap2.js
@@ -1,17 +1,22 @@
-function minimumSwaps(arr) {
-  const n = arr.length;
-  let swapCount = 0;
+// function minimumSwaps(arr) {
+//   const n = arr.length;
+//   let swapCount = 0;
   
-  for (let i = 0; i < n; i++) {
-    while (arr[i] !== i + 1) {
-      const temp = arr[i];
-      const currentIndex = arr[i] - 1;
-      [arr[i], arr[currentIndex]] = [arr[currentIndex], temp];
-      swapCount++;
-    }
-  }
+//   for (let i = 0; i < n; i++) {
+//     while (arr[i] !== i + 1) {
+//       const temp = arr[i]; //4
+//       const currentIndex = arr[i] - 1; 3
+//       [arr[i], arr[currentIndex]] = [arr[currentIndex], temp];
+//       swapCount++;
+//     }
+//   }
 
-  return swapCount;
-}
+//   return swapCount;
+// }
 
-console.log(minimumSwaps([4, 3, 1, 2])); // Output: 3
+// console.log(minimumSwaps([4, 3, 1, 2])); // Output: 3
+const test = [3,1,2,4];
+let c = test[0];
+let b = test[0]-1;
+[test[0], test[b]] = [test[b], c];
+console.log(test)
