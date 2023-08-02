87c848f7df60e489aa5612c970f707a590782f08
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-02 14:51:52 +0900

    문제 해결

--- newYearChaos2.js
+++ newYearChaos2.js
@@ -1,31 +1,17 @@
 function minimumBribes(q) {
-  const n = q.length;
   let swapCount = 0;
-
-  for (let i = 0; i < n; i++) {
-    const bribes = q[i] - (i + 1);
-    console.log(`${bribes}bribes입니다`)
-    if (bribes > 2) {
-      console.log("Too chaotic");
-      return;
-    }
-
-    for (let j = Math.max(0, q[i] - 2); j < i; j++) {
-      if (q[j] > q[i]) {
-        console.log(q[j], q[i])
-        swapCount++;
-      }
-    }
+  const n = q.length;
+  for(let i=0; i<n; i++) {
+      const originalPos = q[i]-(i+1);
+      if(originalPos > 2) {
+          return console.log("Too chaotic");
+      };
+  for(let j=Math.max(0, q[i]-2); j<i; j++) {
+      if(q[j]>q[i]) {
+          [[q[i], q[j]]] = [[q[j], q[i]]];
+          swapCount++;
+      } 
   }
-
-  console.log(swapCount);
-}
-
-// 예제 입력 테스트
-const testCases = [
-  [2, 1, 5, 3, 4]
-];
-
-for (const testCase of testCases) {
-  minimumBribes(testCase);
-}
+  }   console.log(swapCount);
+} 
+minimumBribes([2, 1, 5, 3, 4]);
