8f27bfcc86c0ff62ddfcf8e75af7ffd997886797
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-03 14:06:51 +0900

    for of문과 객체를 활용해 중복 데이터 검증 테스트

--- forIn.js
+++ forIn.js
@@ -1,8 +1,12 @@
-function test(a) {
+function test(a, b) {
   const c = {};
   for(const word in a) {
     c[a]++;
     };
+  for(const word in b) {
+    c[b]++;
+  }
   console.log(c);
 }
-test([1,'2',3]);
+test([1,'2',3], ['안','녕','??']);
+
