41a73fe082e108c59d07266f041a133332791faa
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-03 14:05:19 +0900

    객체 테스트

--- forIn.js
+++ forIn.js
@@ -1,10 +1,8 @@
-const mapTest = new Map();
-const test = [1,2,2,2,2, 2, 3, 4];
-const test2 = [1,2,4];
-test.forEach(data => {
-  mapTest.set(data,1)
-})
-for(const num of test2) {
-  const getTest = mapTest.get(num);
-  console.log(getTest);
+function test(a) {
+  const c = {};
+  for(const word in a) {
+    c[a]++;
+    };
+  console.log(c);
 }
+test([1,'2',3]);
