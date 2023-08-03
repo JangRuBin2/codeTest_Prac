b4449a0b41797f01470da8b627df714779f0d079
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-03 12:38:35 +0900

    hash Map에서 set, get활용 테스트

--- forIn.js
+++ forIn.js
@@ -1,6 +1,10 @@
 const mapTest = new Map();
 const test = [1,2,2,2,2, 2, 3, 4];
+const test2 = [1,2,4];
 test.forEach(data => {
-  mapTest.set(data)
+  mapTest.set(data,1)
 })
-console.log(mapTest)
+for(const num of test2) {
+  const getTest = mapTest.get(num);
+  console.log(getTest);
+}
