52c5030530a0c443d2a29d70bd866cf96a1a07f8
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-08-04 17:36:18 +0900

    Map메서드와 for of문 활용한 해결 -> 아직 이해가 어려움

--- forIn.js
+++ forIn.js
@@ -1,8 +1,25 @@
-function test(a, b) {
-  b.forEach(word=> {
-    // 배열에서 특정 요소가 없으면 -1반환
-    const c = a.indexOf(word);
-    console.log(c)
-  })
+function checkMagazine(magazine, ransomNote) {
+  const wordCount = new Map();
+
+  // 매거진에 있는 단어들을 해시 맵에 저장 (단어와 등장 횟수)
+  for (const word of magazine) {
+    wordCount.set(word, (wordCount.get(word) || 0) + 1);
+  }
+
+  // ransom note의 단어들을 확인하여 매거진에 모두 포함되어 있는지 확인
+  for (const word of ransomNote) {
+    const count = wordCount.get(word);
+    if (!count || count === 0) {
+      console.log('No');
+      return;
+    }
+    wordCount.set(word, count - 1);
+  }
+
+  console.log('Yes');
 }
-test([1, 2, 3, 4], [2, 4])
+
+// 예시
+const magazine = ['give', 'me', 'one', 'grand', 'today', 'night'];
+const ransomNote = ['give', 'one', 'grand', 'today'];
+checkMagazine(magazine, ransomNote); // 출력: Yes
